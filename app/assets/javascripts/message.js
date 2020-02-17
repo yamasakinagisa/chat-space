6c6250126111b7b4ba761556b9a057d70330b170
yamasakinagisa <60636075+yamasakinagisa@users.noreply.github.com>
2020-02-17 16:22:51 +0900

    非同期通信２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function(){ 
     function buildHTML(message){
-    // console.log(message)
      if ( message.image ) {
        var html =
         `<div class="message" data-message-id=${message.id}>
@@ -53,7 +52,6 @@ $('#new_message').on('submit', function(e){
      contentType: false
    })
     .done(function(data){
-      // console.log(data)
       var html = buildHTML(data);
       $('.chat-main__message').append(html);      
       $('form')[0].reset();
